237ca85aafd7a70421943b0b03502736d7fcbe24
Alumno <Alumno@PC32>
2019-09-24 09:42:00 -0500

    conff

---
+++ WpfApp1/WpfApp1/empleado.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class empleado
+    {
+        public string Nombre { get; set;}
+        public string Titulo { get; set;}
+
+        public static empleado GetEmpleado()
+        {
+            var emp = new empleado()
+            {
+                Nombre = "Miguel Escobedo", 
+            Titulo = "todologo"
+            };
+        return emp;
+        }
+    }
+}
